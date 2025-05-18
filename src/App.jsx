46f379f3501f99714d181a20453e748d1e83cf2a
elisha <elishajameel2007@gmail.com>
2025-05-18 03:23:22 -0700

    added font family Space Grotesk

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       try {
         const response = await axios.get(
           "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        );
+        );        
         setCoinsData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import CryptoCard from './CryptoCard'
+import React from "react";
+import CryptoCard from "./CryptoCard";
 
-const CryptoList = ({ coinsData }) => {
+const CryptoList = ({ coinsData, onCoinClick }) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {coinsData.map((coin, index) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {coinsData.map((coin) => (
         <CryptoCard
-          key={index}
+          key={coin.id}
           image={coin.image}
           name={coin.name}
           price={coin.current_price}
+          onClick={() => onCoinClick(coin)}
         />
       ))}
     </div>
   );
-}
+};
 
 export default CryptoList;
