f6a1f5bcf6d172fcf730af0df2c662babcf0f4df
elisha <elishajameel2007@gmail.com>
2025-05-18 23:40:18 -0700

    added setSelected coin in the coins details components to close the details part

--- src/components/CoinsDetails.jsx
+++ src/components/CoinsDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CoinsDetails = ({selectedCoin}) => {
+const CoinsDetails = ({ selectedCoin, setSelectedCoin }) => {
   return (
     <div>
       {selectedCoin && (
